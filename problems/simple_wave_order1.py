work on simple_wave_order1, fixed some bugs - zero-edge meshes now produce a single cell - now properly normalizing normal vector in flux

problems/simple_wave_order1.py
@@ -16,7 +16,10 @@ def step_SG2D(self,dt,stage):
     u = self.fields[uname]
     sigx = self.fields[sxname]
     sigy = self.fields[syname]
-    c = self.fields["c"]
+    if "c" in self.fields:
+        c = self.fields["c"]
+    else:
+        c = None
 
     # int_dom(taux sigxdot) = -int_dom(c^2 u dx(taux))
     #                   +int_bdry(c^2 u taux nx)
@@ -38,7 +41,10 @@ def step_SG2D(self,dt,stage):
         sig_elem = np.empty((Np1,Np1,2))
         sig_elem[:,:,0] = sigx[inds]
         sig_elem[:,:,1] = sigy[inds]
-        c2_elem = c[inds]**2
+        if c is None:
+            c2_elem = elem.fields["c"]
+        else:
+            c2_elem = c[inds]**2
         
         # partial_k phi_{a,b}(x_i,x_j) ; [k,a,b,i,j]
         gradphi = elem.lagrange_grads(
@@ -97,7 +103,10 @@ def step_SG2D(self,dt,stage):
 
 def flux_SG2D(self,stage):
     flux = np.zeros((self.basis_size,3))
-    c = self.fields["c"]
+    if "c" in self.fields:
+        c = self.fields["c"]
+    else:
+        c = None
     uname = "u" if stage == 0 else "u_pred"
     sxname = "sigx" if stage == 0 else "sigx_pred"
     syname = "sigy" if stage == 0 else "sigy_pred"
@@ -144,7 +153,10 @@ def flux_SG2D(self,stage):
         #xcoord for 1,3
         J = np.abs(def_grad[(edgeID+1)%2,(edgeID+1)%2,:])
 
-        c_edge = c[inds]
+        if c is None:
+            c_edge = elem.fields["c"][inds_e[:,0],inds_e[:,1]]
+        else:
+            c_edge = c[inds]
 
         #eigvectors of A:
         eigp = np.empty((elem.degree+1,3)); eigp[:,2] = 1/c_edge
@@ -162,6 +174,8 @@ def flux_SG2D(self,stage):
             #edge 2 or 3; we are on the - side
             eigp[:,:2] *= -1
         
+        eigp[:,:2] /= np.linalg.norm(eigp[:,:2],ord=2,axis=1)[:,np.newaxis]
+
         eign[:,:2] = eigp[:,:2]
 
 
@@ -179,9 +193,6 @@ def flux_SG2D(self,stage):
               + eigp[:,1]*sigy #n2 sigma2
               - c_edge   *u)[:,np.newaxis] #-c u
         )
-
-        if np.max(np.abs(flux)) > 1:
-            1+3
     
     return flux
 ##============end wave flux functions

problems/simple_wave_order2.py
@@ -60,7 +60,10 @@ def step_SG2D(self,dt):
     u = self.fields["u"]
     udot = self.fields["udot"]
     uddot = self.fields["uddot"]
-    c = self.fields["c2"]
+    if "c2" in self.fields:
+        c = self.fields["c2"]
+    else:
+        c = None
 
     u += dt*udot + (0.5*dt**2)*uddot
     udot += (0.5*dt)*uddot #1/2 step before we overwrite
@@ -89,7 +92,10 @@ def step_SG2D(self,dt):
         elem = self.elems[i]
         inds = self.provincial_inds[i]
         u_elem = u[inds]
-        c_elem = c[inds]
+        if c is None:
+            c_elem = elem.fields["c2"]
+        else:
+            c_elem = c[inds]
         gradphi = elem.lagrange_grads(
             np.arange(Np1),np.arange(Np1)[np.newaxis,:],
             np.arange(Np1)[np.newaxis,np.newaxis,:],
@@ -125,6 +131,16 @@ def step_SG2D(self,dt):
 
 ##============end wave step functions
 
+def calc_elem_size(elem):
+    # returns the length of of the largest diagonal
+    a = elem.fields["positions"][0,0,:]
+    b = elem.fields["positions"][-1,0,:]
+    c = elem.fields["positions"][-1,-1,:]
+    d = elem.fields["positions"][0,-1,:]
+    return np.sqrt(max(
+        np.sum((c-a)**2),np.sum((d-b)**2)
+    ))
+
 ##============wave flux functions
 
 def flux_SE2D(self):
@@ -146,7 +162,7 @@ def flux_SE2D(self):
             
             flux[inds[:,0],inds[:,1]] += self.weights * J * bc[1]\
                   * c[inds[:,0],inds[:,1]]
-        if bc[0] == 2: #custom
+        elif bc[0] == 3: #custom
             inds = self.get_edge_inds(edgeID)
             flux[inds[:,0],inds[:,1]] += self.custom_flux(edgeID,*bc[1:])
     
@@ -154,7 +170,10 @@ def flux_SE2D(self):
 
 def flux_SG2D(self):
     flux = np.zeros((self.basis_size))
-    c = self.fields["c2"]
+    if "c2" in self.fields:
+        c = self.fields["c2"]
+    else:
+        c = None
 
     for i,bc in enumerate(self.boundary_conditions):
         #we will enforce dirichlet after the euler step
@@ -172,7 +191,70 @@ def flux_SG2D(self):
             
             flux[basis_inds] += elem.weights * J * bc[1]\
                   * c[basis_inds]
-        if bc[0] == 2: #custom
+        elif bc[0] == 2: #dG
+            #- (2,other_grid,bdry_id,flip,alpha)
+            grid_other = bc[1]
+            bd_id_other = bc[2]
+            flip = bc[3]
+            alpha = bc[4]
+            elemIDself,edgeIDself,_ = self._adjacency_from_int(
+                self.boundary_edges[i])
+            elemself = self.elems[elemIDself]
+
+            localindsself = elemself.get_edge_inds(edgeIDself)
+            provindsself = self.provincial_inds[elemIDself]\
+                                [localindsself[:,0],localindsself[:,1]]
+            
+            elemIDother, edgeIDother,_ = grid_other._adjacency_from_int(
+                grid_other.boundary_edges[bd_id_other])
+            elemother = grid_other.elems[elemIDother]
+            
+            
+            
+            du_self = elemself.bdry_normalderiv(edgeIDself,"u")
+            u_self = self.fields["u"][provindsself]
+            du_other = -elemother.bdry_normalderiv(edgeIDother,"u_prev")
+            provindsother = grid_other.get_edge_provincial_inds(
+                    elemIDother, edgeIDother)
+            u_other = grid_other.fields["u_prev"][provindsother]
+            dudn = 0.5 * (du_self + du_other)
+            if "c2" in self.fields:
+                c = self.fields["c2"][provindsself]
+                cmax_self = np.max(self.fields["c2"]
+                                [self.provincial_inds[elemIDself]])
+            else:
+                c = elemself.fields["c2"][localindsself[:,0],
+                                         localindsself[:,1]]
+                cmax_self = np.max(elemself.fields["c2"])
+            def_grad = elemself.def_grad(localindsself[:,0],
+                                         localindsself[:,1])
+            
+            #not full 2d jacobian; use boundary: ycoord for 0,2
+            #xcoord for 1,3
+            Jw = np.abs(def_grad[(edgeIDself+1)%2,
+                                 (edgeIDself+1)%2,:]) *elemself.weights
+            
+            if "c2" in grid_other.fields:
+                cmax = max(cmax_self,
+                    np.max(grid_other.fields["c2"]
+                        [grid_other.provincial_inds[elemIDother]]))
+            else:
+                cmax = max(cmax_self,
+                    np.max(elemother.fields["c2"]))
+
+            hmax = max(calc_elem_size(elemself),calc_elem_size(elemother))
+
+            # we are looking at Grote et al:
+            # flux terms: int [[u]] . {{c grad v}} + [[v]] . {{c grad u}}
+            #               + a [[u]] . [[v]]
+            flux[provindsself] += \
+                    (Jw * (-elemself.lagrange_deriv(
+                        np.arange(elemself.degree+1),
+                                1, 0)/2 * c * (u_self - u_other)
+                    + dudn * c
+                    - (alpha * cmax/hmax) * (u_self - u_other)))
+            #===================================
+        if bc[0] == 3: #custom
             elemID,edgeID,_ = self._adjacency_from_int(
                 self.boundary_edges[i])
             basis_inds = self.get_edge_provincial_inds(elemID,edgeID)
@@ -199,7 +281,16 @@ def endow_wave(domain):
     - (1,g)
         neumann, set to the value g (can be an array) at each step
         this is in real coordinates. (unless my math is wrong #TODO)
-    - (2,*flags)
+
+    - (2,other_grid,bdry_id,flip,alpha)
+        dG for nodes that line up.
+        other_grid is expected to be an instance of spectral_mesh_2D,
+        with bdry_id being the boundary that lines up with this bdry.
+        flip is a flag that, when set, flips the data first. By default,
+        the directions of increasing parameters point in the same direction.
+        alpha is the constant in Grote et al
+
+    - (3,*flags)
         custom flux scheme (say for discontinuous galerkin). the calculation
         is deferred to a function
             flux[edge] = domain.custom_flux(bdry_id,*flags)
@@ -211,16 +302,16 @@ def endow_wave(domain):
     
     Fields that are used (and need to be set):
     u - domain.fields["u"]
-        the field that is diffused
+        the field that is diffused (only for the first step; gets changed)
     udot - domain.fields["udot"]
-        the rate of change of u
+        the rate of change of u (only for the first step; gets changed)
     uddot - domain.fields["uddot"]
-        the rate of change of udot
+        the rate of change of udot (only for the first step; gets changed)
     c^2 - domain.fields["c2"]
         the constant in front of the laplace operator, representing the
         square of the wave speed
     """
-    if isinstance(domain,SE.spectral_element_2D):
+    if isinstance(domain,SE.spectral_element_2D) and False:
         domain.overwrite_step(step_SE2D)
         Np1 = domain.degree+1
         domain.fields["u"] = np.zeros((Np1,Np1))

sims/order1_wave_expand.py
@@ -8,17 +8,18 @@ import problems.simple_wave_order1 as wave
 import domains.spec_elem as SE
 
 
-
 GSIZEX = 20
 GSIZEY = 20
+
+LEN_RESCALE = 1
 #degree of the elements
 N = 3
 #init_wave: gaussian
 #variance
-sigma2 = 2.5
+sigma2 = 2.5 * LEN_RESCALE**2
 
 #wave speed
-C = 2
+C = 2 * LEN_RESCALE
 
 
 #================================
@@ -30,120 +31,254 @@ display_interval = 10
 
 
 #position of the bottom left of cell (0,0)
-x_offset = -GSIZEX
-y_offset = -GSIZEY/2
+x_offset = -GSIZEX * LEN_RESCALE
+y_offset = -GSIZEY/2 * LEN_RESCALE
 
-wave_centers = [(GSIZEX/3,GSIZEY/4),(-GSIZEX/3,-GSIZEY/4)]
+#wave_centers = [(GSIZEX/3,GSIZEY/4),(-GSIZEX/3,-GSIZEY/4)]
+wave_centers = [(-GSIZEX/3,0)]
+wave_centers = [np.array(p)*LEN_RESCALE for p in wave_centers]
 
-init_wave = lambda x,y: sum((np.exp(((x-cx)**2+(y-cy)**2)/(-2*sigma2)) \
-                    / np.sqrt(sigma2*2*np.pi) for cx,cy in wave_centers))
-
-
-
-waveamp = 1/np.sqrt(sigma2*2*np.pi)
+waveamps = [1/np.sqrt(sigma2*2*np.pi)]
 
 
-grid = np.zeros((GSIZEX,GSIZEY),dtype=bool)
-grid[:,:] = True
-cell_ids = np.zeros((GSIZEX,GSIZEY),dtype=np.uint32)-1
+sensors = [(-GSIZEX/3,0),(-2*GSIZEX/3,0),(0,0),(GSIZEX/3,0),
+           (2*GSIZEX/3,0)]
 
-gridstr = ""
-num_cells = 0
-edges = []
-for j in range(GSIZEY):
-    if j > 0:
-        gridstr += "\n"
-    for i in range(GSIZEX):
-        #gridstr
-        if grid[i,j]:
-            gridstr += "X"
-        else:
-            gridstr += " "
-        if grid[i,j]:
-            #new cell
-            cell_ids[i,j] = num_cells
-            num_cells += 1
+
+
+init_wave = lambda x: sum((np.exp(np.sum(
+    (x - p.reshape(
+        [1 if k > 1 else 2 for k in range(len(x.shape),0,-1)] #broadcast into x
+        ))**2
+    ,-1)/(-2*sigma2)) * a for p,a in zip(wave_centers,waveamps)))
+
+
+
+def build_domain(Lx,Ly,clusters,n,field_vals,
+            xoff = 0, yoff = 0,
+            elem_node_dist = "gll",
+            periodic = False,
+            return_elem_indices = False):
+    
+
+    #elem_node_dist:
+    #  uniform - linspace(0,1,n+1)
+    #  gll     - same as gll nodes
+    if elem_node_dist == "uniform":
+        elem_nodes = np.linspace(0,1,n+1)
+    else:
+        elem_nodes = (1+SE.GLL_UTIL.get_knots(n))*0.5
+
+    num_meshes = np.max(clusters)+1
+    edges = [[] for _ in range(num_meshes)]
+    Nx,Ny = clusters.shape
+
+    elem_hx = Lx/Nx
+    elem_hy = Ly/Ny
+
+    cell_ids = np.zeros((Nx,Ny),dtype=np.uint32)-1
+    num_cells = [0 for _ in range(num_meshes)]
+
+    #build edge arrays for constructing meshes
+    for j in range(Ny):
+        for i in range(Nx):
+            cluster = clusters[i,j]
+            
+            #new cell in cluster
+            cell_ids[i,j] = num_cells[cluster]
+            num_cells[cluster] += 1
             #edges?
-            if i > 0 and grid[i-1,j]:
-                edges.append((cell_ids[i,j],cell_ids[i-1,j],2,0,0))
-            if j > 0 and grid[i,j-1]:
-                edges.append((cell_ids[i,j],cell_ids[i,j-1],3,1,0))
-se_grid = SE.spectral_mesh_2D(N,edges)
-se_grid2 = SE.spectral_mesh_2D(N,edges)
-wave.endow_wave(se_grid)
-wave.endow_wave(se_grid2)
+            if i > 0 and clusters[i-1,j] == cluster:
+                edges[cluster].append((cell_ids[i,j],cell_ids[i-1,j],2,0,0))
+            if j > 0 and clusters[i,j-1] == cluster:
+                edges[cluster].append((cell_ids[i,j],cell_ids[i,j-1],3,1,0))
+    if periodic:
+        # wrap around for periodicity
+        for j in range(Ny):
+            if clusters[0,j] == clusters[-1,j]:
+                edges[clusters[0,j]].append(
+                    (cell_ids[0,j],cell_ids[-1,j],2,0,0))
+        for i in range(Nx):
+            if clusters[i,0] == clusters[i,-1]:
+                edges[clusters[i,0]].append(
+                    (cell_ids[i,0],cell_ids[i,-1],3,1,0))
 
-se_grid.fields["positions"] = np.empty((se_grid.basis_size,2))
-se_grid2.fields["positions"] = np.empty((se_grid2.basis_size,2))
-for j in range(GSIZEY):
-    for i in range(GSIZEX):
-        if grid[i,j]:
-            X_ = np.linspace(i,i+1,N+1)[:,np.newaxis] + x_offset
-            se_grid.elems[cell_ids[i,j]].fields["positions"][:,:,0] = X_
-            se_grid2.elems[cell_ids[i,j]].fields["positions"][:,:,0] = \
-                X_ + GSIZEX
-            Y_ = np.linspace(j,j+1,N+1)[np.newaxis,:] + y_offset
-            se_grid.elems[cell_ids[i,j]].fields["positions"][:,:,1] = Y_
-            se_grid2.elems[cell_ids[i,j]].fields["positions"][:,:,1] = Y_
-
-            se_grid.fields["u"][se_grid.provincial_inds[cell_ids[i,j]]] = \
-                init_wave(X_,Y_)
-            se_grid2.fields["u"][se_grid2.provincial_inds[cell_ids[i,j]]] = \
-                init_wave(X_+GSIZEX,Y_)
+    meshes = [SE.spectral_mesh_2D(n,edges[i]) for i in range(num_meshes)]
+    for mesh in meshes:
+        wave.endow_wave(mesh)
+        mesh.fields["positions"] = np.empty((mesh.basis_size,2))
+    
+    bd_edges = [dict() for _ in range(num_meshes)]
+    #store dg edge adjacencies and position fields
+    for j in range(Ny):
+        for i in range(Nx):
+            cluster = clusters[i,j]
+            mesh = meshes[cluster]
+            cid = cell_ids[i,j]
             
-            se_grid.fields["positions"][
-                se_grid.provincial_inds[cell_ids[i,j]],:] = \
-                se_grid.elems[cell_ids[i,j]].fields["positions"]
-            se_grid2.fields["positions"][
-                se_grid2.provincial_inds[cell_ids[i,j]],:] = \
-                se_grid2.elems[cell_ids[i,j]].fields["positions"]
+            X_ = (elem_nodes+i)[:,np.newaxis] * elem_hx + xoff
+            mesh.elems[cid].fields["positions"][:,:,0] = X_
+            Y_ = (elem_nodes+j)[np.newaxis,:] * elem_hy + yoff
+            mesh.elems[cid].fields["positions"][:,:,1] = Y_
 
-#bd indices for flux; left side
-wall_inds_L = [-1 for _ in range(GSIZEY)]
-for bd in range(se_grid.num_boundary_edges):
-    elemID,edge,_ = se_grid._adjacency_from_int(se_grid.boundary_edges[bd])
-    for j in range(GSIZEY):
-        if elemID == cell_ids[-1,j]:
-            wall_inds_L[j] = bd
-            break
-for bd in range(se_grid2.num_boundary_edges):
-    elemID,edge,_ = se_grid2._adjacency_from_int(se_grid2.boundary_edges[bd])
-    for j in range(GSIZEY):
-        if elemID == cell_ids[0,j]:
-            #link to other side
-            se_grid2.boundary_conditions[bd] = (1, #flux on grid2
-                se_grid,wall_inds_L[j],False)
-            se_grid.boundary_conditions[wall_inds_L[j]] = (1, #flux on grid1
-                se_grid2,bd,False)
-            break
+            mesh.fields["positions"][
+                mesh.provincial_inds[cid],:] = \
+                mesh.elems[cid].fields["positions"]
+
+            #dg edge adjacencies
+            if clusters[i-1,j] != cluster and (i > 0 or periodic):
+                bd_edges[cluster][cid,2] =\
+                    (clusters[i-1,j],cell_ids[i-1,j],0)
+                bd_edges[clusters[i-1,j]][cell_ids[i-1,j],0] =\
+                    (cluster,cid,2)
+            if clusters[i,j-1] != cluster and (j > 0 or periodic):
+                bd_edges[cluster][cid,3] =\
+                    (clusters[i,j-1],cell_ids[i,j-1],1)
+                bd_edges[clusters[i,j-1]][cell_ids[i,j-1],1] =\
+                    (cluster,cid,3)
+
+    bd_ids = [dict() for _ in range(num_meshes)]
+    for meshID,mesh in enumerate(meshes):
+        #populate fields
+        
+        for field in field_vals:
+            mesh.fields[field][:]=field_vals[field](mesh.fields["positions"])
+
+        for i,bd in enumerate(mesh.boundary_edges):
+            elemID,edgeID,_ = mesh._adjacency_from_int(bd)
+            bd_ids[meshID][elemID,edgeID] = i
+            #edges for dG; in bd_edges?
+            if (elemID,edgeID) not in bd_edges[meshID]:
+                continue
+            othermesh,other_elem,other_edge=bd_edges[meshID][elemID,edgeID]
             
-se_grid.fields["c"][:] = C
-se_grid2.fields["c"][:] = C
+            #did we store the ID of the other side?
+            if (other_elem,other_edge) in bd_ids[othermesh]:
+                #create link
+                j = bd_ids[othermesh][other_elem,other_edge]
+                meshes[othermesh].boundary_conditions[j] =\
+                    (1,mesh,i,0)
+                mesh.boundary_conditions[i] =\
+                    (1,meshes[othermesh],j,0)
+    if return_elem_indices:
+        return meshes, cell_ids
+    return meshes
 
 
 import matplotlib.animation
 import matplotlib.pyplot as plt
 from matplotlib import cm
 
-def plot_se_grid_contour():
-    plt.cla()
-    for i,elem in enumerate(se_grid.elems):
-        plt.contour(elem.fields["positions"][:,:,0],
-                    elem.fields["positions"][:,:,1],
-                    se_grid.fields["u"][se_grid.provincial_inds[i]],
-                    cmap=cm.PuBu_r)
+def plot_domain(meshes,title,show=False,save_filename=None,
+                vmin=None,vmax=None, ax = None,
+                use_scatter = False):
+    if ax is None:
+        plt.clf()
+    else:
+        ax.cla()
+    src = plt if ax is None else ax
+    xmin = 0; xmax = 0
+    ymin = 0; ymax = 0
+    
+    umin = 0; umax = 0
+    for mesh in meshes:
+        us = mesh.fields["u"]
+        umin = min(min(us),umin)
+        umax = max(max(us),umax)
+    if vmin is None:
+        vmin = umin
+    if vmax is None:
+        vmax = umax
+    for mesh in meshes:
+        xmin = min(xmin, min(mesh.fields["positions"][:,0]))
+        xmax = max(xmax, max(mesh.fields["positions"][:,0]))
+        ymin = min(ymin, min(mesh.fields["positions"][:,1]))
+        ymax = max(ymax, max(mesh.fields["positions"][:,1]))
+        if use_scatter:
+            bar_= src.scatter(mesh.fields["positions"][:,0],
+                        mesh.fields["positions"][:,1], 1,
+                        mesh.fields["u"],
+                        vmin = vmin, vmax = vmax
+                        )
+        else:
+            for i,elem in enumerate(mesh.elems):
+                bar_ = src.contourf(
+                            elem.fields["positions"][:,:,0],
+                            elem.fields["positions"][:,:,1],
+                    mesh.fields["u"][mesh.provincial_inds[i]],
+                    100,vmin = vmin, vmax = vmax)
+    if ax is None:
+        plt.title(title)
+        plt.xlabel("$x$")
+        plt.ylabel("$y$")
+        plt.xlim((xmin,xmax))
+        plt.ylim((ymin,ymax))
+        plt.colorbar(bar_)
+    else:
+        ax.set_title(title)
+        # ax.set_xlabel("$x$")
+        # ax.set_ylabel("$y$")
+        ax.set_xlim((xmin,xmax))
+        ax.set_ylim((ymin,ymax))
+    if show:
+        plt.show()
+    if save_filename is not None:
+        plt.savefig(save_filename)
 
-def plot_se_grid_scatter():
-    plt.cla()
-    plt.scatter(se_grid.fields["positions"][:,0],
-                se_grid.fields["positions"][:,1],
-                s = 1, c = se_grid.fields["u"],
-                vmin = 0, vmax = waveamp)
-    plt.scatter(se_grid2.fields["positions"][:,0],
-                se_grid2.fields["positions"][:,1],
-                s = 1, c = se_grid2.fields["u"],
-                vmin = 0, vmax = waveamp)
-    plt.title(f"t = {t:.3f}")
+
+
+    
+clusters = np.arange(GSIZEX*2*GSIZEY,dtype=int).reshape((GSIZEX*2,GSIZEY))
+# clusters = np.zeros((GSIZEX*2,GSIZEY),dtype=int)
+# clusters[:GSIZEX,:] = 1
+meshes,cell_ids = build_domain(GSIZEX*2 * LEN_RESCALE,
+                      GSIZEY * LEN_RESCALE,clusters,N,{
+        "u":init_wave,
+        "c":lambda x: C
+    },elem_node_dist="uniform",
+    xoff=x_offset,yoff=y_offset,
+    return_elem_indices=True)
+
+#log locations of the sensors in cell space
+#(cellx,celly,meshID,elemID)
+sensor_cells = np.empty((len(sensors),4),dtype=np.uint32)
+# (localx,localy)
+sensor_local = np.empty((len(sensors),2))
+def bisect(f,a,b,tol):
+    fa = f(a); fb = f(b)
+    if fa == 0:
+        return a
+    if fb == 0:
+        return b
+    while b-a > tol:
+        c = (a+b)*0.5; fc = f(c)
+        if fc == 0:
+            return c
+        elif fa*fc < 0:
+            b = c; fb = fc
+        else:
+            a = c; fa = fc
+    return (a+b)*0.5
+for i,sloc in enumerate(sensors):
+    sx,sy = sloc
+    sensor_cells[i,0] = min(GSIZEX*2-1,max(0,
+        int(np.floor((sx-x_offset)/LEN_RESCALE))))
+    sensor_cells[i,1] = min(GSIZEX*2-1,max(0,
+        int(np.floor((sy-y_offset)/LEN_RESCALE))))
+    sensor_cells[i,2] = clusters[
+        sensor_cells[i,0],sensor_cells[i,1]]
+    sensor_cells[i,3] = cell_ids[
+        sensor_cells[i,0],sensor_cells[i,1]]
+    elem = meshes[sensor_cells[i,2]].elems[sensor_cells[i,3]]
+    # local coords by bisection method;
+    # we're assuming independence of each axis
+    sensor_local[i,0] = bisect(
+        lambda x: elem.reference_to_real(x,0)[0],
+        -1,1,1e-8)
+    sensor_local[i,1] = bisect(
+        lambda y: elem.reference_to_real(0,y)[1],
+        -1,1,1e-8)
+
 
 num_frames = int(tmax / (dt*display_interval))
 
@@ -153,15 +288,31 @@ fig, ax = plt.subplots(figsize=(5,3))
 frameskip = int(np.round(0.05/dt))
 
 t = 0
+t_vals = np.arange(num_frames) * (dt * display_interval)
+sensor_vals = np.empty((len(sensors),num_frames))
+
+
 def animate(i):
     global t
     #plot_se_grid_contour()
-    plot_se_grid_scatter()
+    plot_domain(meshes,f"t = {t:10.4f}",show=False,
+                ax=ax,use_scatter=True,vmin=0,vmax=max(waveamps)*.1)
+    for k in range(len(sensors)):
+        mesh = meshes[sensor_cells[k,2]]
+        u = mesh.fields["u"][mesh.provincial_inds[sensor_cells[k,3]]]
+        elem = mesh.elems[sensor_cells[k,3]]
+        # u^{ij} L_i(local_x) L_j(local_y)
+        sensor_vals[k,i] = np.einsum("ij,ik,jl,k,l->",
+                u,elem.lagrange_polys,elem.lagrange_polys,
+                sensor_local[k,0]**np.arange(elem.degree+1),
+                sensor_local[k,1]**np.arange(elem.degree+1))
+    if i == num_frames-1:
+        return
     for _ in range(display_interval):
-        se_grid.step(dt,0)
-        se_grid2.step(dt,0)
-        se_grid.step(dt,1)
-        se_grid2.step(dt,1)
+        for mesh in meshes:
+            mesh.step(dt,0)
+        for mesh in meshes:
+            mesh.step(dt,1)
         t += dt
         print(f"frame: {i:05d}; t = {t:10.4f}",end="\r")
 
@@ -170,3 +321,13 @@ anim = matplotlib.animation.FuncAnimation(fig, animate,
 savestr = "outputs/tmp/order1wave_expand.mp4"
 anim.save(filename=os.path.join(os.path.dirname(directory),
             savestr), writer="ffmpeg")
+
+plt.figure(figsize=(10,6))
+plt.clf()
+for k in range(len(sensors)):
+    plt.plot(t_vals,sensor_vals[k,:],
+        label=f"Sensor {k} ({sensors[k][0]:.2f},{sensors[k][1]:.2f})")
+plt.legend()
+savestr = "outputs/tmp/order1wave_expand_sensors.png"
+plt.savefig(os.path.join(os.path.dirname(directory),
+            savestr))